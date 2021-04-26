2e8b191381024238860ba815c9c5d6a3a432c906
ks2019575015 <48661711+ks2019575015@users.noreply.github.com>
2021-04-26 10:52:37 +0900

    Add files via upload

---
+++ 02 DerivedConstructorApp.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace HiddenNameApp
+{
+    class BaseClass
+    {
+        public BaseClass()
+        {
+            Console.WriteLine("BaseClass Constructor ...");
+        }
+    }
+    class DerivedClass : BaseClass
+    {
+        public DerivedClass()
+        {
+            Console.WriteLine("DerivedClass Constructor ...");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DerivedClass obj = new DerivedClass();
+            Console.WriteLine("In the main ...");
+        }
+    }
+}
